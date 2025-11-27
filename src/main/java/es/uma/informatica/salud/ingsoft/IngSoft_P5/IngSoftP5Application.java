dccc4a7e146b30a5d64f7859ff577e99661fc044
soyRosana <rosanagfortes@uma.es>
2025-11-27 10:01:20 +0100

    feat:first commit

---
+++ src/main/java/es/uma/informatica/salud/ingsoft/IngSoft_P5/IngSoftP5Application.java
@@ -0,0 +1,13 @@
+package es.uma.informatica.salud.ingsoft.IngSoft_P5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngSoftP5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngSoftP5Application.class, args);
+	}
+
+}
